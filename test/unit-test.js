c771834275f3741d7f8ecdd780e51a75056ccbf3
Luiskr1993 <luiskr1993@gmial.com>
2020-12-07 18:36:47 -0600

    Cambio para que la prueba unitaria falle

--- test/unit-test.js
+++ test/unit-test.js
@@ -5,6 +5,6 @@ var calculadora = require('../src/public/js/funciones');
 describe('calculando anticipo sugerido respecto de un precio de venta', ()=>{
     it('deberá retornar 200 (20%) de 1,000 que es el precio de venta utilizado', ()=>{
         const anticipo = calculadora.anticipo(1000);
-        expect(anticipo).to.be.equal("200.00");
+        expect(anticipo).to.be.equal("200.01");
     })
 })
